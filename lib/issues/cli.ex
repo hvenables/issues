c8526eaaeb8acc94eb13d8dd3efddc92302f93f5
Harry Venables <harry@tagadab.com>
2019-09-09 18:31:27 +0100

    Refactored parse_args and installed httpoison

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -8,7 +8,9 @@ defmodule Issues.CLI do
   """
 
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args
+    |> process
   end
   @doc"""
   `argv` can be -h or --help, which returns :help.
@@ -18,17 +20,31 @@ defmodule Issues.CLI do
   """
 
   def parse_args(argv) do
-    parse = OptionParser.parse(argv, switches: [help: :boolean],
-                                     aliases:  [h: :help])
+    OptionParser.parse(argv, switches: [help: :boolean], aliases:  [h: :help])
+    |> elem(1)
+    |> args_to_internal_representation
+  end
 
-    case parse do
-      { [help: true], _, _ }
-        -> :help
-      { _, [ user, project, count ], _ }
-        -> { user, project, String.to_integer(count) }
-      { _, [ user, project ], _ }
-        -> { user, project, @default_count }
-      _ -> :help
-    end
+  defp args_to_internal_representation([user, project, count]) do
+    { user, project, String.to_integer(count) }
+  end
+
+  defp args_to_internal_representation([user, project]) do
+    { user, project, @default_count }
+  end
+
+  defp args_to_internal_representation(_) do
+    :help
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [ count | #{@default_count} ]
+    """
+    System.halt(0)
+  end
+
+  def process({user, project, _count}) do
+    Issues.GithubIssues.fetch(user, project)
   end
 end
